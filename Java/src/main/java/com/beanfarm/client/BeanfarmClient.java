51fa8220f550f354d5500c59b78c47880bf66488
Cameron Worthington <cameron.worthington@bbd.co.za>
2024-03-12 21:30:50 +0200

    fixed access token passing

--- Java/src/main/java/com/beanfarm/client/BeanfarmClient.java
+++ Java/src/main/java/com/beanfarm/client/BeanfarmClient.java
@@ -1,6 +1,6 @@
 package com.beanfarm.client;
 
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 import com.beanfarm.model.BeanfarmResponse;
@@ -8,5 +8,5 @@ import com.beanfarm.model.BeanfarmResponse;
 public interface BeanfarmClient {
 
     @GetExchange("api/employees")
-    BeanfarmResponse random(@PathVariable(name = "access_token") String token);
+    BeanfarmResponse random(@RequestParam(name = "access_token") String token);
 }
